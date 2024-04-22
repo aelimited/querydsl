dce761c0323ce0bc045d8e09efd8f3df5e1628b8
aelimited <cloim09@gmail.com>
2024-04-22 18:24:34 +0900

    ADD 검색, 페이징, 정렬, 조회 테스트 추가 정렬에서 desc = 내림차순, asc = 오름차순 헷갈리지 않기..!

--- src/test/java/study/querydsl/entity/MemberTest.java
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -1,5 +1,6 @@
 package study.querydsl.entity;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -10,6 +11,9 @@ import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.*;
 
 
 @SpringBootTest
@@ -55,16 +59,82 @@ class MemberTest {
 //        QMember m = new QMember("m"); //별칭을 사용하는 방법임
         if (queryFactory != null) {
             Member findMember = queryFactory
-                    .select(QMember.member)
-                    .from(QMember.member)
-                    .where(QMember.member.userName.eq("MemberA"))
+                    .select(member)
+                    .from(member)
+                    .where(member.userName.eq("MemberA"))
                     .fetchOne();
 
             if (findMember != null) {
-                Assertions.assertThat(findMember.getUserName()).isEqualTo("MemberA");
+                assertThat(findMember.getUserName()).isEqualTo("MemberA");
             } else {
                 System.out.println(" = ");
             }
+        }
+    }
+
+    @Test
+    public void search() {
+        if (queryFactory != null) {
+            Member findMember = queryFactory
+                    .select(member)
+//                    .where(member.userName.eq("Member1")
+//                            .and(member.age.eq(10)))
+                    //and 조건을 파라미터로 처리 하는 방법
+                    .where(member.userName.eq("member1"),
+                            member.age.eq(10))
+                    .fetchOne();
+
+            assertThat(findMember.getUserName()).isEqualTo("Member1");
+        }
+    }
+
+    @Test
+    public void resultFetch() {
+        if (queryFactory != null) {
+            QueryResults<Member> results = queryFactory
+                    .selectFrom(member)
+                    .fetchResults();
+
+            results.getTotal();
+            List<Member> members = results.getResults();
+        }
+    }
+
+    @Test
+    public void sort() {
+        if (queryFactory != null) {
+            em.persist(new Member(null, 100));
+            em.persist(new Member("member5", 100));
+            em.persist(new Member("member6", 100));
+            List<Member> result = queryFactory
+                    .selectFrom(member)
+                    .where(member.age.eq(100))
+                    .orderBy(member.age.desc(),
+                            member.userName.asc().nullsLast())
+                    .fetch();
+            Member member5 = result.get(0);
+            Member member6 = result.get(1);
+            Member memberNull = result.get(2);
+            assertThat(member5.getUserName()).isEqualTo("member5");
+            assertThat(member6.getUserName()).isEqualTo("member6");
+            assertThat(memberNull.getUserName()).isNull();
+        }
+    }
+
+    @Test
+    public void paging() {
+        if (queryFactory != null) {
+            QueryResults<Member> queryResults = queryFactory
+                    .selectFrom(member)
+                    .orderBy(member.userName.desc())
+                    .offset(1)
+                    .limit(2)
+                    .fetchResults();
+
+            assertThat(queryResults.getTotal()).isEqualTo(4);
+            assertThat(queryResults.getLimit()).isEqualTo(2);
+            assertThat(queryResults.getOffset()).isEqualTo(1);
+            assertThat(queryResults.getResults().size()).isEqualTo(2);
         }
     }
 }
